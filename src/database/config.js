d61eae33d96d356edd435d46d5e7a81a45a65eb7
LinaKind <lina.kinduryte@gmail.com>
2021-10-31 18:18:56 +0000

    env module set up

--- src/database/config.js
+++ src/database/config.js
@@ -1,16 +1,17 @@
 import * as firebase from 'firebase'
-
+//import firebase from 'firebase/app'
+import 'firebase/auth'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBxGZ3h85pOAuoSqcioThgX5R-EEvohAIM",
-    authDomain: "photwall-6493d.firebaseapp.com",
-    databaseURL: "https://photwall-6493d-default-rtdb.firebaseio.com",
-    projectId: "photwall-6493d",
-    storageBucket: "photwall-6493d.appspot.com",
-    messagingSenderId: "1032337447611",
-    appId: "1:1032337447611:web:f846893068f39e81e9e79e",
-    measurementId: "G-S1MGQQMTWJ"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_PROJECT_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
   
   };
   
